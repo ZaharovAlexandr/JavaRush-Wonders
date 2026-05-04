af72ee710d96b743bc15e40e076bafaebf7dadc2
Alexandr Zakharov <zaharof.sasha2014@yandex.ru>
2026-05-04 14:20:57 +0300

    GalleryFix + Faqs

--- script.js
+++ script.js
@@ -51,3 +51,22 @@ returnDate.addEventListener('change', function() {
     returnSpan.classList.add('booking__input-placeholder-date_active')
     returnSpan.textContent = returnDate.value;
 })
+
+
+
+
+
+const buttonFaqs = document.querySelectorAll('.faqs__button')
+const imageFaqs = document.querySelectorAll('.faqs__image')
+const textFaqs = document.querySelectorAll('.faqs__qustion-text')
+
+
+buttonFaqs.forEach((element, i) => {
+    element.addEventListener('click', function() {
+    imageFaqs[i].classList.toggle('faqs__image_active')
+    textFaqs[i].classList.toggle('faqs__qustion-text_active')
+})
+});
+
+
+
