b5bf8113bd4be8c4c4af91e347c2dec87d6917ee
Alexandr Zakharov <zaharof.sasha2014@yandex.ru>
2026-05-04 17:27:46 +0300

    FoooterFull + Station realisation

---
+++ station.js
@@ -0,0 +1,87 @@
+const Stations = [
+    "Zermatt Bus Terminal",
+    "Interlaken Ost Bus Station",
+    "Grindelwald Bus Terminal",
+    "Lauterbrunnen Bahnhof",
+    "Lucerne Bahnhofquai",
+    "Chamonix-Mont-Blanc Sud (France, near Swiss border)",
+    "Geneva Bus Station",
+    "Bern PostAuto Terminal",
+    "Gstaad Bus Station",
+    "St. Moritz Bahnhof PostAuto",
+    "Verbier Village",
+    "Davos Platz Postautohaltestelle",
+    "Andermatt Gotthardpass",
+    "Täsch Bahnhof (Shuttle to Zermatt)",
+    "Flims Dorf Post",
+    "Chamonix Sud Bus Station",
+    "Annecy Gare Routière",
+    "Grenoble Gare Routière",
+    "Nice Airport (Bus to Alps)",
+    "Bourg-Saint-Maurice Gare Routière",
+    "Morzine Gare Routière",
+    "Les Gets Gare Routière",
+    "Val d'Isère Centre",
+    "Courchevel 1850",
+    "Megève Place du Village",
+    "Aosta Autostazione",
+    "Bolzano Autostazione",
+    "Trento Autostazione",
+    "Cortina d'Ampezzo Autostazione",
+    "Bormio Bus Station",
+    "Livigno Centro",
+    "Merano Autostazione",
+    "Sestriere Bus Stop",
+    "Ortisei (St. Ulrich) Autostazione",
+    "Canazei Piazza Marconi",
+    "Innsbruck Hauptbahnhof Bus Terminal",
+    "Salzburg Süd Busbahnhof",
+    "Mayrhofen Bahnhof",
+    "Lech am Arlberg Postamt",
+    "Kitzbühel Hahnenkammbahn",
+    "Ischgl Seilbahn",
+    "Zell am See Postplatz",
+    "Bad Gastein Bahnhof",
+    "St. Anton am Arlberg Bahnhof",
+    "Sölden Postamt",
+    "Garmisch-Partenkirchen Bahnhof (Bus Station)",
+    "Berchtesgaden Busbahnhof",
+    "Oberstdorf Busbahnhof",
+    "Füssen Bahnhof (Bus Station)",
+    "Mittenwald Bahnhof (Bus Station)",
+    "Bled Bus Station",
+    "Bohinj Jezero",
+    "Kranjska Gora Avtobusna Postaja"
+];
+
+const departureButton = document.querySelector('.booking__station-input')
+const buttonHelper = document.querySelectorAll('.booking__station-help-button');
+
+buttonHelper.forEach(element => {
+    element.addEventListener('mousedown', (e) => {
+        e.preventDefault();
+    })
+})
+
+buttonHelper.forEach(element => {
+    element.addEventListener('click', function(){
+        departureButton.value = element.textContent;
+        document.querySelector('.booking__station-help').classList.toggle('booking__station-help_active')
+    })
+});
+
+departureButton.addEventListener('input', function() {
+    let b = Stations.filter(item => {
+        if ((item.toLowerCase()).includes(departureButton.value.toLowerCase())) {
+            return true;
+        } 
+    })
+    for (let i = 0; i < buttonHelper.length; i++) {
+        buttonHelper[i].textContent = b[i];
+    }
+})
+
+departureButton.addEventListener('focus', function() {
+    document.querySelector('.booking__station-help').classList.toggle('booking__station-help_active')
+})
+
