d3e3f906fc1afb4c28975de711b1ce7d841b97e0
Alexandr Zakharov <zaharof.sasha2014@yandex.ru>
2026-05-07 18:21:26 +0300

    BurgerMenu + Logic

---
+++ burgerMenu.js
@@ -0,0 +1,6 @@
+const burgerbutton = document.querySelector('.header__burger-menu');
+const burgerMenu = document.querySelector('.burgermenu')
+
+burgerbutton.addEventListener('click', function() {
+    burgerMenu.classList.toggle('burgermenu_active')
+})
